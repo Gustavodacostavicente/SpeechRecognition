964fd5d775c897a28680103b6709ebc2bc77b652
gustavodacostavicente <gustavodacostavicente@gmail.com>
2021-12-31 15:13:32 -0300

    integracao

--- Speech.py
+++ Speech.py
@@ -1,34 +1,75 @@
 import speech_recognition as sr
+import pyautogui
+import webbrowser
+import sys
 
-#Função para ouvir e reconhecer a fala
-def ouvir_microfone():
-    #Habilita o microfone do usuário
-    microfone = sr.Recognizer()
+def Esperar_Comando():
+    Mic_Rec_Comando = sr.Recognizer()
+    with sr.Microphone() as Source:
+        Mic_Rec_Comando.adjust_for_ambient_noise(Source)
+        print("Diga o Comando: ")
+        Audio_Comando = Mic_Rec_Comando.listen(Source)
+        try:
+            Frase_Comando = Mic_Rec_Comando.recognize_google(Audio_Comando, language='pt-BR')
+            Comando = Frase_Comando.lower()
+
+            if 'abrir navegador' in Comando:
+                url = 'https://docs.python.org/'
+                webbrowser.open_new(url)
+                Esperar_Comando()
+                
+            if 'abrir youtube' in Comando:
+                url = 'https://www.youtube.com/'
+                webbrowser.open_new(url)
+                Esperar_Comando()
+                
+            if 'abrir google' in Comando:
+                url = 'https://www.google.com/'
+                webbrowser.open_new(url)
+                Esperar_Comando()
+                
+            if 'encerrar' in Comando:
+                print("Sistema Encerrado")
+                sys.exit()
+                
+        except sr.UnkownValueError:
+            print("Não entendi")
+            
+        return Frase_Comando
+
+
+def Testar_Integracao():
     
-    #usando o microfone
-    with sr.Microphone() as source:
-        
-        #Chama um algoritmo de reducao de ruidos no som
-        microfone.adjust_for_ambient_noise(source)
-        
-        #Frase para o usuario dizer algo
-        print("Diga alguma coisa: ")
-        
-        #Armazena o que foi dito numa variavel
-        audio = microfone.listen(source)
-        
-    try:
-        
-        #Passa a variável para o algoritmo reconhecedor de padroes
-        frase = microfone.recognize_google(audio,language='pt-BR')
-        
-        #Retorna a frase pronunciada
-        print("Você disse: " + frase)
-        
-    #Se nao reconheceu o padrao de fala, exibe a mensagem
-    except sr.UnkownValueError:
-        print("Não entendi")
-        
-    return frase
+    try:  
+        Mic_Rec = sr.Recognizer()
+        with sr.Microphone() as Source:
+            Mic_Rec.adjust_for_ambient_noise(Source)
+            print("Teste o Microfone: ")
+            Audio_Integracao = Mic_Rec.listen(Source)
+            
+        try:
+            Frase_Integracao = Mic_Rec.recognize_google(Audio_Integracao, language='pt-BR')
+            Comando_Integracao = Frase_Integracao.lower()
+            print("Você disse: " + Frase_Integracao)
+            
+            if 'teste de integração' in Comando_Integracao:
+                print('Teste de Integração com Sucesso.')
+                Executar_Comando()
 
-ouvir_microfone()
+        except sr.UnkownValueError:
+            print("Erro Comunicação")
+            
+        return Frase_Integracao
+                
+                
+    except:
+        print("Integração Perdida")
+                
+        
+
+
+def Executar_Comando():
+    Esperar_Comando()
+    
+
+Testar_Integracao()
